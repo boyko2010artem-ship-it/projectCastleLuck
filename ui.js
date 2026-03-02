44bc3af53484053f19500d811ad5dcac1d30aea9
boyko2010artem-ship-it <boyko2010artem@gmail.com>
2026-03-02 17:45:40 +0300

    Refactor update method for player and enemy bars

--- ui.js
+++ ui.js
@@ -1,8 +1,17 @@
 const UI = {
 
 update(){
-playerBar.style.width=(Game.player.hp/Game.player.maxHp*100)+"%";
-enemyBar.style.width=(Game.enemy.hp/Game.enemy.maxHp*100)+"%";
+let pPercent=(Game.player.hp/Game.player.maxHp)*100;
+let ePercent=(Game.enemy.hp/Game.enemy.maxHp)*100;
+
+playerBar.style.width=pPercent+"%";
+enemyBar.style.width=ePercent+"%";
+
+if(pPercent<30) playerBar.classList.add("low");
+else playerBar.classList.remove("low");
+
+if(ePercent<30) enemyBar.classList.add("low");
+else enemyBar.classList.remove("low");
 
 playerMorale.innerText=Game.player.morale;
 enemyMorale.innerText=Game.enemy.morale;
@@ -11,6 +20,11 @@ playerTP.innerText=Game.player.tp;
 
 log(text){
 log.innerText=text;
+},
+
+shake(){
+document.body.classList.add("shake");
+setTimeout(()=>document.body.classList.remove("shake"),300);
 }
 
 };
