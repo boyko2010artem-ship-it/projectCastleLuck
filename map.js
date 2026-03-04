fe55c7d490f100584c4ceff5789248b718d41546
boyko2010artem-ship-it <boyko2010artem@gmail.com>
2026-03-04 13:54:50 +0300

    Update map.js

--- map.js
+++ map.js
@@ -2,20 +2,6 @@ const MapSystem={
 
 render(){
 
-let map=document.getElementById("map")
-
-let html="<div class='map'>"
-
-Game.campaign.forEach((s,i)=>{
-
-html+=`<div class="stage ${i===Game.stage?'current':''}">⚔</div>`
-
-})
-
-html+="</div>"
-
-map.innerHTML=html
-
 }
 
 }
