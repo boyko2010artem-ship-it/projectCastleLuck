7621584dc4d41d33484b522770398d8fefc5c4fd
boyko2010artem-ship-it <boyko2010artem@gmail.com>
2026-03-04 13:41:20 +0300

    Update achievements.js

--- achievements.js
+++ achievements.js
@@ -1,40 +1,19 @@
 const Achievements={
 
 list:{
-
-firstWin:{
-name:"Первая победа",
-desc:"Вы выиграли первую битву"
+firstWin:"Первая победа",
+rocket:"Мастер ракет"
 },
 
-rocketMaster:{
-name:"Мастер ракет",
-desc:"Использовать ракету 5 раз"
-},
-
-campaignWin:{
-name:"Полководец",
-desc:"Завершить кампанию"
-},
-
-strategist:{
-name:"Стратег",
-desc:"Выиграть битву за 5 ходов"
-}
-
-},
-
-unlocked:{},
+unlocked:JSON.parse(localStorage.getItem("achievements")||"{}"),
 
 unlock(id){
 
-if(this.unlocked[id]) return
+if(this.unlocked[id])return
 
 this.unlocked[id]=true
 
-UI.showAchievement(this.list[id])
-
-SaveSystem.save()
+localStorage.setItem("achievements",JSON.stringify(this.unlocked))
 
 }
 
