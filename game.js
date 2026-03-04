81b8ba63c815a062f8fdd4e78721e34341a7cc40
boyko2010artem-ship-it <boyko2010artem@gmail.com>
2026-03-04 13:27:30 +0300

    Update game.js

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ campaign:null,
 currentCampaign:null,
 stage:0,
 
+army:null,
+armyKey:null,
+
 player:{hp:100,tp:0,morale:50},
 enemy:{hp:100,tp:0,morale:50},
 
@@ -15,6 +18,27 @@ start(type){
 
 this.currentCampaign=type
 this.campaign=Data.campaigns[type]
+
+this.chooseArmy()
+
+},
+
+chooseArmy(){
+
+let list=Object.keys(Data.armies)
+
+let text="Выберите армию:\n"
+
+list.forEach((a,i)=>{
+text+=`${i+1}. ${Data.armies[a].name}\n`
+})
+
+let choice=prompt(text)
+
+let key=list[choice-1]
+
+this.armyKey=key
+this.army=Data.armies[key]
 
 menu.classList.add("hidden")
 game.classList.remove("hidden")
@@ -29,44 +53,68 @@ loadStage(){
 
 stageTitle.innerText=this.campaign[this.stage].title
 
-this.player={hp:100,tp:0,morale:50}
-this.enemy={hp:100,tp:0,morale:50}
+this.player={
+hp:this.army.hp,
+tp:0,
+morale:50
+}
 
-this.stats={turns:0,damageDealt:0,damageTaken:0}
+this.enemy={
+hp:100,
+tp:0,
+morale:50
+}
+
+this.stats={
+turns:0,
+damageDealt:0,
+damageTaken:0
+}
 
 UI.update()
 UI.renderMap()
+
+SaveSystem.save()
 
 },
 
 playerAction(type){
 
-let dmg=12
+let dmg=this.army.damage
 
 if(type==="attack"){
+
 this.enemy.hp-=dmg
 this.stats.damageDealt+=dmg
+
 UI.log("⚔ "+dmg)
+
 }
 
 if(type==="heal" && this.player.tp>=1){
+
 this.player.hp+=10
 this.player.tp--
+
 }
 
 if(type==="rocket" && this.player.tp>=2){
+
 this.enemy.hp-=30
 this.player.tp-=2
-this.stats.damageDealt+=30
-UI.log("🚀 30")
+
+Achievements.unlock("rocketMaster")
+
 }
 
 this.player.tp++
+
 this.stats.turns++
 
 if(Math.random()<0.25){
 
 let e=Events[Math.floor(Math.random()*Events.length)]
+
 e.effect()
 
 }
@@ -89,37 +137,20 @@ aiTurn(){
 
 let dmg=10+this.campaign[this.stage].difficulty*2
 
-if(this.enemy.hp<30 && this.enemy.tp>=1){
-
-this.enemy.hp+=12
-this.enemy.tp--
-
-UI.log("AI лечится")
-
-}else if(this.enemy.tp>=2){
-
-this.player.hp-=28
-this.enemy.tp-=2
-
-UI.log("AI 🚀")
-
-}else{
-
 this.player.hp-=dmg
-
-UI.log("AI ⚔ "+dmg)
-
-}
 
 this.enemy.tp++
 
 this.stats.damageTaken+=dmg
+
+UI.log("AI ⚔ "+dmg)
 
 UI.update()
 
 if(this.player.hp<=0){
 
 alert("Вы проиграли")
+
 location.reload()
 
 }
@@ -132,7 +163,12 @@ this.stage++
 
 if(this.stage>=this.campaign.length){
 
+Achievements.unlock("campaignWin")
+
 alert("Кампания завершена")
+
+SaveSystem.clear()
+
 location.reload()
 
 }else{
@@ -147,16 +183,20 @@ this.loadStage()
 
 window.Game=Game
 
+
 victoryBtn.onclick=()=>{
 
 victoryScreen.classList.add("hidden")
+
 UI.showHistory(Game.campaign[Game.stage])
 
 }
 
+
 continueBtn.onclick=()=>{
 
 modal.classList.add("hidden")
+
 Game.nextStage()
 
 }
